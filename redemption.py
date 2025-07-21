dcc9fedd7b9547236f9dcd93bb43a6a6667000cc
aaron <aaron@MBP16.local>
2025-07-21 06:21:06 -0700

    Fix: Correct Chrome binary path

--- redemption.py
+++ redemption.py
@@ -12,7 +12,7 @@ import time
 import logging
 
 options = Options()
-options.binary_location = "/usr/bin/chromium-browser"
+options.binary_location = "/usr/bin/google-chrome"
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
