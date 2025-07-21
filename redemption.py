e1941c0f7ee3a516a522acc57525b8980b3aa567
aaron <aaron@MBP16.local>
2025-07-21 06:42:07 -0700

    import os

--- redemption.py
+++ redemption.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
