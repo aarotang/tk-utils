f68e2adc993752b4e5b53cd0fba6e86f0c285640
aaron <aaron@MBP16.local>
2025-07-21 06:29:30 -0700

    Fix: import shutil

--- redemption.py
+++ redemption.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import shutil
 import logging
 
 options = Options()
